7423d393f1f2d89987807ff7749d8f3f7b590745
Samar Srivastava <samarsrivastava44@gmail.com>
2018-05-04 01:36:59 +0530

    hudukchullu updates

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Samar Srivastava">
     <link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
+     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <script src="js/bootstrap.min.js"></script>
 
     <style>
